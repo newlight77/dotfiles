69d19097a0210c018e808bf97b4c8353afbb3930
Kong To <newlight77@gmail.com>
2018-05-24 18:22:59 +0200

    doc(bash): update readme and rename profile/.bashrc_path to profile/.bashrc_vars

--- customize.sh
+++ customize.sh
@@ -15,16 +15,14 @@ if [ ! -f ~/.git-completion.bash ]; then
   curl -L https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.bash_custom/.git-completion.bash
 fi
 
-mkdir -p ~/..bash_custom
+mkdir -p ~/.bash_custom
 cp  ${DIR}/profile/.bashrc_natelandau ~/.bash_custom/.bashrc_natelandau
 cp  ${DIR}/profile/.bashrc_profile    ~/.bash_custom/.bashrc_profile
 cp  ${DIR}/profile/.bashrc_alias      ~/.bash_custom/.bashrc_alias
 cp  ${DIR}/profile/.bashrc_ps12       ~/.bash_custom/.bashrc_ps12
 cp  ${DIR}/profile/.gitconfig_alias   ~/.bash_custom/.gitconfig_alias
 cp  ${DIR}/profile/.bashrc            ~/.bash_custom/.bashrc
-if [ -z $NPM_PACKAGES ]; then
-  cp  ${DIR}/profile/.bashrc_path       ~/.bash_custom/.bashrc_path
-fi
+cp  ${DIR}/profile/.bashrc_vars       ~/.bash_custom/.bashrc_vars
 
 cat ${DIR}/profile/.bashrc_profile    >> ~/.bashrc
 cat ${DIR}/profile/.gitconfig         >> ~/.gitconfig
